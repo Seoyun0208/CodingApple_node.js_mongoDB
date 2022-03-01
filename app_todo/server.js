ff785ef604d5b943ae27f7f9b7ab90ac2f017b75
Seoyun <sylph0105@naver.com>
2022-03-01 13:21:07 +0900

    [Update] app_todo. Bootstrap 을 이용해 빠르게 UI 개발해보기

--- app_todo/server.js
+++ app_todo/server.js
@@ -5,14 +5,10 @@ app.listen(8080, function(){
     console.log('listening on 8080');
 });
 
-app.get('/pet', function(req, res){
-    res.send('펫용품 쇼핑 페이지입니다.');
-});
-
-app.get('/beauty', function(req, res){
-    res.send('뷰티용품 쇼핑 페이지입니다.');
-});
-
 app.get('/', function(req, res){
     res.sendFile( __dirname + '/index.html');
+});
+
+app.get('/write', function(req, res){
+    res.sendFile(__dirname + '/write.html');
 });
