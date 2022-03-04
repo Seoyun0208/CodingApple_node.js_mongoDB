05d497ce01143c5757487ff872d9305a618181fd
Seoyun <sylph0105@naver.com>
2022-03-04 11:06:30 +0900

    [Update] app_todo. Database에 자료 저장하는 법

--- app_todo/server.js
+++ app_todo/server.js
@@ -4,7 +4,18 @@ const app = express();
 app.use(express.urlencoded({extended: true}));
 
 const MongoClient = require('mongodb').MongoClient;
+
+var db;
+
 MongoClient.connect('mongodb+srv://admin:qwer1234@cluster0.e8wvg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', function(err, client){
+    if(err) {return console.log(err)};
+
+    db = client.db('app_todo');
+
+    db.collection('post').insertOne({name : 'seoyun', age : 20}, function(err, result){
+        console.log('저장완료');
+    });
+
     app.listen(8080, function(){
         console.log('listening on 8080');
     });
