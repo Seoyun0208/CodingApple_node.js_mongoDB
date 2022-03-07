6497d2a54cc0f5b8ee560c2751423bcbea756bd4
Seoyun <sylph0105@naver.com>
2022-03-07 15:13:22 +0900

    [Update] app_todo. HTML 에 DB 데이터 넣어보기_2

--- app_todo/server.js
+++ app_todo/server.js
@@ -15,24 +15,38 @@ MongoClient.connect('mongodb+srv://admin:qwer1234@cluster0.e8wvg.mongodb.net/myF
         console.log('listening on 8080');
     });
 
-    app.post('/add', function(req, res){
-        res.send('전송완료');
-        
-        db.collection('post').insertOne({할일: req.body.content, 마감일 : req.body.date}, function(err, result){
-            console.log('데이터 저장 완료!')
-        })
-    })
 })
 
+
+// ! Home
 app.get('/', function(req, res){
     res.sendFile( __dirname + '/index.html');
 });
 
+
+// ! Write
 app.get('/write', function(req, res){
     res.sendFile(__dirname + '/write.html');
 });
 
+
+// ! List
 app.get('/list', function(req, res){
-    res.render('list.ejs')
+
+    db.collection('post').find().toArray(function(err, result){
+        // console.log(result);
+        res.render('list.ejs', {posts : result});
+    });
+
 });
+
+
+// * add
+app.post('/add', function(req, res){
+    res.send('전송완료');
+    
+    db.collection('post').insertOne({할일: req.body.content, 마감일 : req.body.date}, function(err, result){
+        console.log('데이터 저장 완료!')
+    })
+})
 
