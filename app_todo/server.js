a3192d01f65f2c38e8a551c1f9c86e649b64a0b3
Seoyun <sylph0105@naver.com>
2022-03-10 22:34:56 +0900

    [Update] app_todo. 글 수정 기능 만들기

--- app_todo/server.js
+++ app_todo/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 app.use(express.urlencoded({extended: true}));
 const MongoClient = require('mongodb').MongoClient;
+const methodeOverride = require('method-override');
+app.use(methodeOverride('_method'));
 app.set('view engine', 'ejs');
 app.use('/public', express.static('public')); // static 파일 보관을 목적으로 public 폴더를 사용하겠다는 의미
 
@@ -37,7 +39,7 @@ app.get('/list', function(req, res){
 
     db.collection('post').find().toArray(function(err, result){
         // console.log(result);
-        res.render('list.ejs', {posts : result});
+        res.render('list.ejs', {post : result});
     });
 
 });
@@ -66,8 +68,8 @@ app.post('/add', function(req, res){
         });
     });
 
-    res.send('게시물 추가 완료');
-    
+    // res.send('게시물 추가 완료');
+    res.redirect('/list') 
 })
 
 
@@ -84,10 +86,34 @@ app.delete('/delete', function(req, res){
 
 
 // * detail
+// 누군가가 더보기 버튼을 클릭하여 /detail 로 GET 요청을 하면
 app.get('/detail/:id', function(req, res){
+    // DB 에서 게시글 불러오기
     req.params.id = parseInt(req.params.id);
     db.collection('post').findOne({_id : req.params.id}, function(err, result){
         console.log(result);
         res.render('detail.ejs', {detail : result});
     })
 });
+
+
+// * edit
+// 누군가가 수정 버튼을 클릭하여 /edit 으로 GET 요청을 하면
+app.get('/edit/:id', function(req, res){
+    // DB 에서 게시글 불러오기
+    req.params.id = parseInt(req.params.id);
+    db.collection('post').findOne({_id : req.params.id}, function(err, result){
+        console.log(result);
+        res.render('edit.ejs', {edit : result});
+    })
+})
+
+// 누군가가 폼에서 /edit 으로 PUT 요청을 하면
+app.put('/edit', function(req, res){
+    // 폼에 기재된 데이터를 db.collection 에 업데이트하기
+    req.body.id = parseInt(req.body.id);
+    db.collection('post').updateOne({_id : req.body.id}, {$set : {할일: req.body.content, 마감일 : req.body.date}}, function(){
+        console.log('게시물 수정 완료');
+        res.redirect('/list') 
+    })
+})
