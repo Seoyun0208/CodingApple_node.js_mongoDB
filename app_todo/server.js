12eb3819e385ebfeeadd74cf53e8a709a7561f9e
Seoyun <sylph0105@naver.com>
2022-03-10 12:17:13 +0900

    [Update] app_todo. AJAX 로 삭제요청하기_2

--- app_todo/server.js
+++ app_todo/server.js
@@ -44,7 +44,6 @@ app.get('/list', function(req, res){
 // * add
 // 누군가가 폼에서 /add 로 POST 요청을 하면
 app.post('/add', function(req, res){
-    res.send('전송 완료');
 
     // 게시물마다 번호를 달아 저장하기
     // DB.counter 내에서 '총게시물개수'를 찾아서
@@ -56,20 +55,28 @@ app.post('/add', function(req, res){
 
         // 이제 DB.post 에 번호를 단 새로운 게시물을 추가
         db.collection('post').insertOne({_id : totalPosts + 1, 할일: req.body.content, 마감일 : req.body.date}, function(err, result){
-            console.log('데이터 저장 완료!');
+            console.log('게시물 저장 완료!');
 
             // DB.counter 의 '총게시물개수' 의 totalPosts 값을 1씩 증가
             db.collection('counter').updateOne({name : '총게시물개수'},{ $inc : {totalPosts: 1} }, function(err, result){
-                console.log('데이터 업데이트 완료!');
+                console.log('총 게시물 개수 업데이트 완료!');
             });
         });
     });
+
+    res.send('게시물 추가 완료');
     
 })
 
 // * delete
+// 누군가가 삭제 버튼을 클릭하여 /delete 로 DELETE 요청을 하면
 app.delete('/delete', function(req, res){
     // DB 에서 게시글 삭제하기
-    res.send('삭제 완료')
+    req.body._id = parseInt(req.body._id);
+    db.collection('post').deleteOne(req.body, function(err, result){
+        console.log('게시물 삭제 완료');
+    })
+    
+    res.send('게시물 삭제 완료');
 })
 
